a73d8908f8eae11fe875e557aa784c2037089871
vy vo <tuonggvy.vo@gmail.com>
2019-11-25 04:19:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,12 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = []
-  array << "e"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
   array = []
-  array.unshift ("f")
+  array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
